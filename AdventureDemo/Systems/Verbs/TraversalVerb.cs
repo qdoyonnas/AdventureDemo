5139471787394d01bf6428f0b4f5f950789fb998
Quentin Doyonnas <qdoyonnas@gmail.com>
2019-11-09 14:09:17 -0500

    (WIP) TraversalVerb.

---
+++ AdventureDemo/Systems/Verbs/TraversalVerb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureDemo
+{
+    class TraversalVerb : Verb
+    {
+        public TraversalVerb( GameObject self )
+            : base(self)
+        {
+
+        }
+
+        public override void Action( GameObject target )
+        {
+
+        }
+
+        public override CheckResult Check( GameObject target )
+        {
+            
+        }
+    }
+}
