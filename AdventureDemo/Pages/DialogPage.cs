c09826bbf09e37ad97414f5f0522aa18621fcc55
Quentin Doyonnas <qdoyonnas@gmail.com>
2020-11-25 21:39:52 -0500

    First-pass dialog page.

--- AdventureDemo/Pages/DialogPage.cs
+++ AdventureDemo/Pages/DialogPage.cs
@@ -7,13 +7,29 @@ namespace AdventureDemo
 {
 	class DialogPage : WaywardEngine.ContentPage
 	{
+		StackPanel optionsPanel;
+
 		public DialogPage() : base()
         {
-			SetTitle("Wait Time");
+			SetTitle("Dialogue"); // This should always be replaced by the calling object
 
             FrameworkElement panel = GameManager.instance.GetResource<FrameworkElement>("DialogPanel");
             AddContent(panel);
+
+			optionsPanel = WaywardEngine.Utilities.FindNode<StackPanel>(panel, "Options");
         }
+
+		public delegate void EntryActionDelegate();
+		public void AddEntry(string text, EntryActionDelegate action)
+		{
+			FrameworkElement entry = GameManager.instance.GetResource<FrameworkElement>("DialogEntry");
+			optionsPanel.Children.Add(entry);
+
+			TextBlock entryText = WaywardEngine.Utilities.FindNode<TextBlock>(entry, "Text");
+			entryText.Text = text;
+
+			entry.MouseLeftButtonUp += delegate { action(); CloseAction(); };
+		}
 
 		public override void Clear()
 		{
--- AdventureDemo/Systems/Verbs/WaitVerb.cs
+++ AdventureDemo/Systems/Verbs/WaitVerb.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Documents;
 using WaywardEngine;
 
 namespace AdventureDemo
@@ -21,19 +22,28 @@ namespace AdventureDemo
         public override bool Action(GameObject target)
         {
             // Open dialog for choosing time
-            WaywardManager.instance.AddPage(new DialogPage(), WaywardManager.instance.GetRelativeWindowPoint(0.5, 0.5));
+            DialogPage dialogPage =  new DialogPage();
+            dialogPage.SetTitle("Wait Time");
 
+            dialogPage.AddEntry("100", () => { WaitAction(100); });
+            dialogPage.AddEntry("500", () => { WaitAction(500); });
+            dialogPage.AddEntry("1000", () => { WaitAction(1000); });
+
+            WaywardManager.instance.AddPage(dialogPage, WaywardManager.instance.GetRelativeWindowPoint(0.5, 0.5));
             return true;
         }
 
-        protected bool WaitAction()
+        protected bool WaitAction(double duration)
         {
             // Create data dictionary to be passed to observers
             Dictionary<string, object> data = new Dictionary<string, object>();
 
+            actionTime = duration;
+
             // Message for Verbose pages
-            data["message"] = WaywardTextParser.ParseAsBlock($"[0] {displayLabel.ToLower()}.",
-                () => { return self.GetData("name top").span; }
+            data["message"] = WaywardTextParser.ParseAsBlock($"[0] {displayLabel.ToLower()} for [1].",
+                () => { return self.GetData("name top").span; },
+                () => { return WaywardTextParser.Parse(actionTime.ToString()); }
             );
             data["turnPage"] = true;
             data["displayAfter"] = true;
@@ -65,7 +75,7 @@ namespace AdventureDemo
                 bool success = double.TryParse(e.parameters[0], out waitTime);
                 if( !success ) { return false; }
 
-                WaitAction();
+                WaitAction(waitTime);
             }
 
             return true;
